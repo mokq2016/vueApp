1048753e84fbba7391d69467b22a73585f6140a6
mokq2016 <873302396@qq.com>
2017-08-02 09:23:43 +0800

    add index-menu

---
+++ .svn/pristine/c4/c40b6ca1960b0f726ab34440c16769b6540d1267.svn-base
@@ -0,0 +1,60 @@
+<template>
+    <div class="index-banner">
+          
+          <div class="top-bar">
+            <span class="search-box">
+              <input type="text" />
+              <i class="icon iconfont icon-search"></i>
+            </span>
+            <span class="icon iconfont icon-scan"></span>
+          </div>
+        
+        </div>
+</template>
+<script>
+    export default{
+
+    }
+</script>
+<style scoped>
+    .index-banner{
+    height: 153px;
+    background: #ddd;
+    background: url(../../../static/index/index_banner.png);
+    background-size: 375px;
+  }
+  .top-bar{
+    width: 82%;
+    padding-right: 37px;
+    box-sizing:border-box;
+    -moz-box-sizing:border-box; /* Firefox */
+    -webkit-box-sizing:border-box; /* Safari */
+    position: relative;
+    margin: 0 auto;
+    padding-top: 10px;
+  }
+  .icon-scan, .icon-search{
+    position: absolute;
+    color: #fff;
+  }
+  .icon-search{
+    top: 0px;
+    right: 5px;
+  }
+  .icon-scan{
+    font-size: 22px;
+    right: 0px;
+    top: 9px;
+  }
+  .search-box{
+    position: relative;
+  }
+  .search-box input{
+    width: 100%;
+    border-radius: 10px;
+    line-height: 24px;
+    text-indent: 10px;
+    background: rgba(255, 255, 255, .6);
+    color: #666;
+  }
+</style>
---
+++ src/views/index/footerMenu.vue
@@ -0,0 +1,101 @@
+<template>
+  <div>
+    <tabbar>
+      <tabbar-item link="/index/ywbl" :class="{'weui-bar__item_on':$route.path == '/index/ywbl'}">
+        <i slot="icon" class="ywbl footer-ionic"></i>
+        <span slot="label">业务办理</span>
+      </tabbar-item>
+      <tabbar-item link="/index/fpgl" :class="{'weui-bar__item_on':$route.path == '/index/fpgl'}">
+        <i slot="icon" class="fpgl footer-ionic"></i>
+        <span slot="label">发票管理</span>
+      </tabbar-item>
+      <tabbar-item link="/index/sscx" :class="{'weui-bar__item_on':$route.path == '/index/sscx'}">
+        <i slot="icon" class="sscx footer-ionic"></i>
+        <span slot="label">涉税查询</span>
+      </tabbar-item>
+      <tabbar-item link="/index/aboutMe" :class="{'weui-bar__item_on':$route.path == '/index/aboutMe'}">
+        <i slot="icon" class="smgl footer-ionic"></i>
+        <span slot="label">我</span>
+      </tabbar-item>
+    </tabbar>
+  </div>
+</template>
+<script>
+import {
+  Tabbar,
+  TabbarItem,
+  Group,
+  Cell
+} from 'vux';
+
+export default {
+  components: {
+    Tabbar,
+    TabbarItem,
+    Group,
+    Cell
+  },
+  data() {
+    return {
+
+    }
+  },
+
+  props: {},
+
+  methods: {
+    changeTab: function(a, b, c) {
+      alert(0)
+      console.log(a, b, c)
+    }
+  },
+  watch:{
+
+  }
+}
+</script>
+<style scoped="">
+.ywbl {
+  background: url(../../../static/index/ywbl.png) 0 0 no-repeat;
+}
+
+.fpgl {
+  background: url(../../../static/index/fpgl.png) 0 0 no-repeat;
+}
+
+.sscx {
+  background: url(../../../static/index/sscx.png) 0 0 no-repeat;
+}
+
+.smgl {
+  background: url(../../../static/index/smgl.png) 0 0 no-repeat;
+}
+
+.weui-bar__item_on .ywbl {
+  background: url(../../../static/index/ywbl_on.png) 0 0 no-repeat;
+}
+
+.weui-bar__item_on .fpgl {
+  background: url(../../../static/index/fpgl_on.png) 0 0 no-repeat;
+}
+
+.weui-bar__item_on .sscx {
+  background: url(../../../static/index/sscx_on.png) 0 0 no-repeat;
+}
+
+.weui-bar__item_on .smgl {
+  background: url(../../../static/index/smgl_on.png) 0 0 no-repeat;
+}
+
+.footer-ionic,
+.weui-bar__item_on .footer-ionic {
+  display: block;
+  width: 24px;
+  height: 24px;
+  background-size: 24px;
+}
+
+.weui-bar__item_on .weui-tabbar__label span {
+  color: #1286fe;
+}
+</style>
